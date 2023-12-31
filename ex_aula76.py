ab96011c0ea8e377b8c10e3c112bb4cc9cd1ead1
Pedrow1406 <150269200+Pedrow1406@users.noreply.github.com>
2023-12-31 20:47:41 -0300

    Atualização Terminada no Exercicio da aula 76

--- ex_aula76.py
+++ ex_aula76.py
@@ -8,36 +8,37 @@ Faça a contagem de tentativas do seu usuário.
 """
 
 secret_word = input('Digite a Palavra Secreta: ').upper() #Input para o Usuario da Definir a Palavra Secreta
-cripto_word_list = [] #Lista para criptografar a secret_word
-for percorrer in secret_word:# Para cada letra da palavra secreta
-    cripto_word_list.append('*') #Adicone um '*' para os * se adaptarem ao tamanho da palavra secreta
-trys_list = [] #Lista de Tentativas
-trys_erro = 0 # Quantidade de Tentativas Erradas
-correct_word = 0 # Quantidade de Acertos
-while True:
-    trys_list = [] #Lista de Tentativas Resetada apos cada intereção do loop
-    letra = input('Digite uma letra e tente acertar a palavra secreta: ').upper()
+secret_letters = ["*" for each_letter in range(len(secret_word))] #Lista para criptografar a secret_word
 
-    if len(letra) > 1: #Se o usuario digitar mais de uma letra.
+fail_count = 0 # Quantidade de Tentativas Erradas
+correct_len = 0 # Quantidade de Letras corretas
+while True:
+    letter = input('Digite uma letra e tente acertar a palavra secreta: ').upper()
+
+    if len(letter) > 1: #Se o usuario digitar mais de uma letra.
         print('Digite apenas 1 letra por vez!')
         continue
-    if len(letra) == 0: #Se o usuario não digitar nada.
+    if len(letter) == 0: #Se o usuario não digitar nada.
         print("Digite alguma letra!")
         continue
-    for indice, c in enumerate(secret_word): #Vai percorrer a string secret_word
-        trys_list.append(c) #Vai adicionar cada letra da palavra secret a lista de tentativa
-        if c == letra: #Se a letra da palavra secreta for igual a letra que o usuario digitou
-            correct_word += 1 #A varivel correct_word recebe mais 1 a cada letra correta
-            cripto_word_list[indice] = c # E se a letra for correta o item lista criptografada vira a letra
-            trys_list.remove(c) #Se a letra for correta ele remove a letra correta da lista de tentativa
-            for cd_cripto in cripto_word_list: #Para cada * da lista criptografada
-                print(cd_cripto, end=' ') #Ele vai printar a letra e tirar a quebra de linha
-            print() #print vazio para quebrar de linha para nao interferir nos outros prints
-        elif c != letra: #Se a cada letra da lista da secret_word for diferente da letra que o usario digitou
-            if len(trys_list) == len(secret_word): # E se a quantidade de itens da trys_list for igual a quantidade de itens da secret_word
-                trys_erro += 1 #Try vai receber mais 1 ou seja soma 1 tentativa a cada erro
-                print(f'Letra Incorreta. Tentativa ({trys_erro}x)') #printa que a letra esta incorreta e mostra a quant de trys
 
-    if correct_word == len(secret_word): #Se a quantidade de letras acertadas for igual a quantidade de letras da palavra secreta
-        print(f'Parabéns!!! Você Descobriu a Palavra Secreta com {trys_erro} tentativas.') #Vai printar um Parabens com a quantidade de Trys
+    #Realiza uma busca pela letra na palavra
+    found = False 
+    for i, c in enumerate(secret_word): #Vai percorrer a string secret_word
+        if c == letter: #Se a letra da palavra secreta for igual a letra que o usuario digitou
+            correct_len += 1 #A varivel correct_len recebe mais 1 a cada letra correta
+            secret_letters[i] = c # E se a letra for correta o item lista criptografada vira a letra
+            found = True 
+    
+    #Verifica o resultado da busca
+    if found:
+        for cd_cripto in secret_letters: #Para cada * da lista criptografada
+                    print(cd_cripto, end=' ') #Ele vai printar a letra e tirar a quebra de linha
+        print() #adiciona quebra de linha
+    else:
+        fail_count += 1 #Try vai receber mais 1 ou seja soma 1 tentativa a cada erro
+        print(f'Letra Incorreta. Contagem de erros em ({fail_count}x)') #printa que a letra esta incorreta e mostra a quant de trys
+        
+    if correct_len == len(secret_word): #Se a quantidade de letras acertadas for igual a quantidade de letras da palavra secreta
+        print(f'Parabéns!!! Você Descobriu a Palavra Secreta com {fail_count} tentativas falhas.') #Vai printar um Parabens com a quantidade de Trys
         break # E depois vai se encerrar o loop while e vai terminar o programa.
