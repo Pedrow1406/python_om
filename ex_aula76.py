6d113b79e999dc74f825506cc133897cb8d82779
Pedrow1406 <150269200+Pedrow1406@users.noreply.github.com>
2023-12-31 19:49:49 -0300

    Update no Ex da aula 76

--- ex_aula76.py
+++ ex_aula76.py
@@ -17,21 +17,21 @@ correct_word = 0 # Quantidade de Acertos
 while True:
     trys_list = [] #Lista de Tentativas Resetada apos cada intereção do loop
     letra = input('Digite uma letra e tente acertar a palavra secreta: ').upper()
-    
+
     if len(letra) > 1: #Se o usuario digitar mais de uma letra.
         print('Digite apenas 1 letra por vez!')
         continue
     if len(letra) == 0: #Se o usuario não digitar nada.
         print("Digite alguma letra!")
         continue
-    for posicao, c in enumerate(secret_word): #Vai percorrer a string secret_word
+    for indice, c in enumerate(secret_word): #Vai percorrer a string secret_word
         trys_list.append(c) #Vai adicionar cada letra da palavra secret a lista de tentativa
         if c == letra: #Se a letra da palavra secreta for igual a letra que o usuario digitou
             correct_word += 1 #A varivel correct_word recebe mais 1 a cada letra correta
-            cripto_word_list[posicao] = c # E se a letra for correta o item lista criptografada vira a letra
+            cripto_word_list[indice] = c # E se a letra for correta o item lista criptografada vira a letra
             trys_list.remove(c) #Se a letra for correta ele remove a letra correta da lista de tentativa
-            for cd_letra in cripto_word_list: #Para cada letra da lista criptografada
-                print(cd_letra, end=' ') #Ele vai printar a letra e tirar a quebra de linha
+            for cd_cripto in cripto_word_list: #Para cada * da lista criptografada
+                print(cd_cripto, end=' ') #Ele vai printar a letra e tirar a quebra de linha
             print() #print vazio para quebrar de linha para nao interferir nos outros prints
         elif c != letra: #Se a cada letra da lista da secret_word for diferente da letra que o usario digitou
             if len(trys_list) == len(secret_word): # E se a quantidade de itens da trys_list for igual a quantidade de itens da secret_word
