22e6fe888f37a8f1f01a1243fc30c8db9885c63b
Pedrow1406 <150269200+Pedrow1406@users.noreply.github.com>
2024-01-01 18:33:38 -0300

    Fazendo o exercise da aula 86

---
+++ ex_aula86.py
@@ -0,0 +1,6 @@
+# Exiba os indices de cada item da lista ao lado do nome de cada item da lista sem usar a função enumerate
+
+lista = ['Maria', 'Helena', 'Luiz', 'Pedrow']
+for each_item in range(len(lista)):
+    print(each_item, lista[each_item])
+
