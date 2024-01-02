2be70001d3ea238a115d01cc4f79601ac5888fd5
Pedrow1406 <150269200+Pedrow1406@users.noreply.github.com>
2024-01-01 21:27:35 -0300

    Fazendo o exercise da aula 90
    Fiz sozin :flushed:

---
+++ ex_aula90.py
@@ -0,0 +1,81 @@
+"""
+Faça uma lista de compras com listas
+O usuário deve ter a possibilidade de 
+inserir, apagar e listar valores da sua lista
+Não permita que o programa quebre com 
+erros de índices inexistentes na lista
+"""
+import os
+compras = []
+def mostra_lista():
+    for cada_produto in range(len(compras)):
+        print(f'{cada_produto} {compras[cada_produto]}')
+while True:
+    produto = input('Qual produto você quer comprar: ').strip().capitalize()
+    compras.append(produto)
+    continua = input('Voce quer continuar comprando [S/N]: ').strip().upper()
+    resp = ''
+    loops = 0
+    if continua not in 'SN':
+        print('Digite apenas [S] ou [N]')
+        continue
+    elif continua in 'SN':
+        if 'S' in continua:
+            continue
+        else:
+            while True:
+                os.system('cls')
+                if len(compras) == 0:
+                    print('Sua lista de compras esta vazia no momento.')
+                    resp = input('Voce deseja comprar algum produto [S/N]: ').strip().upper()
+                    if resp not in 'SN':
+                        print('Digite apenas [S] ou [N]')
+                        continue
+                    elif resp in 'SN':
+                        break
+                else:
+                    print('Sua lista de compras atual é: ')
+                    mostra_lista()
+                if loops > 0: #So vai entrar no loop se o loop for maior que 0. Pq no loop 0 essas variaveis ainda não existem
+                    if 'S' in deletar:
+                        produto_deletar = input('Qual produto voce deseja deletar?(Digite o Numero do Produto): ').strip()
+                        if produto_deletar.isdigit():
+                            produto_deletar = int(produto_deletar)
+                            if produto_deletar >= len(compras):
+                                print('Não existe um produto com esse numero.')
+                            else:
+                                compras.pop(produto_deletar)
+                                os.system('cls')
+                                mostra_lista()
+                        else:
+                            print('Porfavor digite o Numero do Produto.')
+                deletar = input('Deseja deletar algum item da sua lista [S/N]: ').strip().upper()
+                if deletar not in 'SN':
+                    print('Digite apenas [S] ou [N]')
+                else:
+                    if 'S' in deletar:
+                        loops += 1
+                        continue
+                    if 'N' in deletar:
+                        break
+                if 'S' in resp or 'N' in resp:
+                    break
+    if len(compras) > 0:
+        resp = input('Voce deseja comprar mais algum produto [S/N]: ').strip().upper()
+        if resp not in 'SN':
+            print('Digite apenas [S] ou [N]')
+            continue
+        elif resp in 'SN':
+            if 'S' in resp:
+                continue
+
+    if 'N' in deletar or 'N' in resp:
+        os.system('cls')
+        print('Compra Encerrada!')
+        if len(compras) == 0:
+            print('Infelizmente você decidiu não comprar nada.')
+            break
+        print('Sua lista de compras foi: ')
+        mostra_lista()
+        break
+print('Tenha um Bom Dia!')
