874df2aaea47d6d15d7af696534b7aa54be68c69
Pedrow1406 <150269200+Pedrow1406@users.noreply.github.com>
2024-01-03 20:47:17 -0300

    Aula 119 OM

---
+++ secao_4/aula/aula_119.py
@@ -0,0 +1,12 @@
+pessoa = {
+    'nome': 'Pedrow',
+    'sobrenome': 'Melo',
+    'idade': 18,
+    'altura': 1.65,
+    'endereco': [
+        {'rua': 'João Pessoa', 'numero': 2411},
+        {'rua': 'Marechal', 'numero': 520},
+    ],
+}
+
+print(f'{pessoa['nome']} tem {pessoa['idade']} anos.')
