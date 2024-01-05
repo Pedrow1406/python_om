71959db1794fb0bd88390ccdc4889183df0ddfe1
Pedrow1406 <150269200+Pedrow1406@users.noreply.github.com>
2024-01-05 16:04:48 -0300

    Update ex_aula131.py

--- secao_4/exercicios/ex_aula131.py
+++ secao_4/exercicios/ex_aula131.py
@@ -7,6 +7,7 @@ Requisistos:
     Exemplo:
         [1, 2, 3, 3, 2, 1] -> 1, 2 e 3 são duplicados (retorne 3)
         [1, 2, 3, 4, 5, 6] -> Retorne -1  (Não tem duplicados)
+        [1, 4, 9, 8, -> 9 <-, 4, 8] Retorne 9
     Se não encontrar duplicados na lista, retorne -1
 """
 lista_de_listas_de_inteiros = [
@@ -15,7 +16,8 @@ lista_de_listas_de_inteiros = [
     [1, 3, 2, 4, 8, 6, 5, 9, 6, 7],
     [4, 8, 8, 8, 5, 10, 6, 7 , 5, 2],
     [3,2, 3, 2, 5, 6, 5, 6, 7, 8, 7],
-    [10, 9, 8 , 7, 6, 5, 4, 3, 2 ,1]
+    [10, 9, 8 , 7, 6, 5, 4, 3, 2 ,1],
+    [1, 4, 9, 8, 9,  4, 8]
 ]
 def itens_duplicados(lista):
     numeros = []
@@ -25,11 +27,18 @@ def itens_duplicados(lista):
             numeros.append(cada_item)
         elif cada_item in numeros:
             duplicados.add(cada_item)
+    duplicados = list(duplicados)
+    if len(duplicados) > 1:
+        print(f'O {duplicados[1]} foi a segunda ocorrência dos valores duplicados')
     if not duplicados:
         return -1
     return len(duplicados)
 
+def linha():
+    print('-=' * 25)
+
 for cada_lista in lista_de_listas_de_inteiros:
+    linha()
     duplicado = itens_duplicados(cada_lista)
     if duplicado == -1:
         print(f'A lista {cada_lista} não repetiu nenhum valor')
