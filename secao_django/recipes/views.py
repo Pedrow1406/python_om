d4efde611a08945675cdd027c9813b36957f8a79
Pedrow1406 <150269200+Pedrow1406@users.noreply.github.com>
2024-01-22 18:18:29 -0300

    partial

--- secao_django/recipes/views.py
+++ secao_django/recipes/views.py
@@ -3,10 +3,4 @@ from django.http import HttpResponse
 
 # Create your views here.
 def home(request): # Recebe uma request 
-    return render(request, 'recipes/home.html')
-
-def sobre(request): # Recebe uma request 
-    return render(request, 'recipes/sobre.html')
-
-def contato(request): # Recebe uma request 
-    return render(request, 'recipes/contato.html')
+    return render(request, 'recipes/pages/home.html')
