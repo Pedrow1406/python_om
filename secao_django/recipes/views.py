ca4016e418bb09b5aeec4f9e5132dca6a6d41904
Pedrow1406 <150269200+Pedrow1406@users.noreply.github.com>
2024-01-23 12:35:09 -0300

    Injetando dados fakes e aleatorios na lista da view

--- secao_django/recipes/views.py
+++ secao_django/recipes/views.py
@@ -1,9 +1,11 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-
+from .util.recipes.factory import make_recipe
 # Create your views here.
 def home(request): # Recebe uma request 
-    return render(request, 'recipes/pages/home.html')
+    return render(request, 'recipes/pages/home.html', context={
+        'recipes': [make_recipe() for _ in range(20)]
+    })
 
 def recipes(request, id): # Recebe uma request 
-    return render(request, 'recipes/pages/recipe-view.html')
+    return render(request, 'recipes/pages/recipe-view.html', context={'recipe':make_recipe})
