6893dbd213b9ad83f5b2e7373082ceb0634e763c
Pedrow1406 <150269200+Pedrow1406@users.noreply.github.com>
2024-01-23 11:21:12 -0300

    Herança em templates e template blocks

--- secao_django/recipes/views.py
+++ secao_django/recipes/views.py
@@ -4,3 +4,6 @@ from django.http import HttpResponse
 # Create your views here.
 def home(request): # Recebe uma request 
     return render(request, 'recipes/pages/home.html')
+
+def recipes(request, id): # Recebe uma request 
+    return render(request, 'recipes/pages/recipe-view.html')
