4fc7c3becdced3788678ce90b6bfd74f826ff553
Pedrow1406 <150269200+Pedrow1406@users.noreply.github.com>
2024-01-25 16:55:42 -0300

    Criando meu banco de dados de Recipes e Cartegory

--- secao_django/recipes/models.py
+++ secao_django/recipes/models.py
@@ -3,16 +3,20 @@ from django.contrib.auth.models import User
 # Create your models here.
 
 class Category(models.Model):
-    name = models.CharField(max_lenght=65)
+    name = models.CharField(max_length=65)
 
-class  Recipe(models.Model):
+    def __str__(self):
+        return self.name
+    
+    
+class Recipe(models.Model):
     title = models.CharField(max_length=65)
-    description = models.CharField(max_lenght=165)
+    description = models.CharField(max_length=165)
     slug = models.SlugField()
     preparation_time = models.IntegerField()
-    preparation_time_unit = models.CharField(max_lenght=65)
+    preparation_time_unit = models.CharField(max_length=65)
     servings = models.IntegerField()
-    servings_unit = models.CharField(max_lenght=65)
+    servings_unit = models.CharField(max_length=65)
     preparation_step = models.TextField()
     preparation_step_is_html = models.BooleanField(default=False)
     created_at = models.DateField(auto_now_add=True)
@@ -21,3 +25,6 @@ class  Recipe(models.Model):
     cover = models.ImageField(upload_to='recipes/covers/%Y/%m/%d/')
     category = models.ForeignKey(Category, on_delete=models.SET_NULL, null=True)
     author = models.ForeignKey(User, on_delete=models.SET_NULL, null=True)
+
+    def __str__(self):
+        return self.title
